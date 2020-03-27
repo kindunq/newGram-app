678dd58ca115e03568c7f3f5502713f8405994c9
kindunq <humanb2box@gmail.com>
2020-03-27 14:08:46 +0900

    docs: login check

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import { Ionicons } from '@expo/vector-icons';
 import { AppLoading } from 'expo';
 import * as Font from 'expo-font';
 import { Asset } from 'expo-asset';
-import { Text, View, AsyncStorage } from 'react-native';
+import { Text, View, AsyncStorage, TouchableOpacity } from 'react-native';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { persistCache } from 'apollo-cache-persist';
 import ApolloClient from 'apollo-boost';
@@ -32,25 +32,52 @@ export default function App() {
 				...apolloClientOptions
 			});
 			const isLoggedIn = await AsyncStorage.getItem('isLoggedIn');
-			if (isLoggedIn === null || isLoggedIn === false) {
-				setIsLoggedIn(false);
+			if (isLoggedIn === null || isLoggedIn === 'false') {
+				setIsLoggedIn();
 			} else {
 				setIsLoggedIn(true);
 			}
 			setLoaded(true);
 			setClient(client);
 		} catch (error) {
-			console.log;
+			console.log(error);
 		}
 	};
 	useEffect(() => {
 		preLoad();
 	}, []);
+
+	const logUserIn = async () => {
+		try {
+			await AsyncStorage.setItem('isLoggedIn', 'true');
+			setIsLoggedIn(true);
+		} catch (error) {
+			console.log(error);
+		}
+	};
+	const logUserOut = async () => {
+		try {
+			await AsyncStorage.setItem('isLoggedIn', 'false');
+			setIsLoggedIn(false);
+		} catch (error) {
+			console.log(error);
+		}
+	};
 	return loaded && client && !isLoggedIn !== null ? (
 		<ApolloProvider client={client}>
 			<ThemeProvider theme={style}>
-				<View>
-					{isLoggedIn === true ? <Text>I'm In</Text> : <Text>I'm Out</Text>}
+				<View
+					style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}
+				>
+					{isLoggedIn === true ? (
+						<TouchableOpacity onPress={logUserOut}>
+							<Text>I'm In</Text>
+						</TouchableOpacity>
+					) : (
+						<TouchableOpacity onPress={logUserIn}>
+							<Text>I'm Out</Text>
+						</TouchableOpacity>
+					)}
 				</View>
 			</ThemeProvider>
 		</ApolloProvider>
